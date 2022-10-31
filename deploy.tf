53cbc00d584fe57575d313fff68af711eb3792f2
Bryan Boettcher <brboett@gmail.com>
2022-10-31 15:47:47 -0400

    fixes

--- deploy.tf
+++ deploy.tf
@@ -19,9 +19,5 @@ resource "docker_image" "di_ado_agent" {
 resource "docker_container" "dc_ado_agent" {
   name  = "ado_agent"
   image = docker_image.di_ado_agent.image_id
-  env = [
-    "AZP_URL=${AZP_URL}", 
-    "AZP_TOKEN=${AZP_TOKEN}", 
-    "AZP_POOL=${AZP_POOL}"
-  ]
+  env = ["AZP_URL=${AZP_URL}", "AZP_TOKEN=${AZP_TOKEN}", "AZP_POOL=${AZP_POOL}"]
 }
